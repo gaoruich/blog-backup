32c70604e7d3a2f9b9a6b21e786ed8758a6d036a
gaoruich <2310064298@qq.com>
2019-02-21 14:46:56 +0800

    modify post

---
+++ upload.bat
@@ -0,0 +1,6 @@
+git config --local user.name "gaoruich"
+git config --local user.email "2310064298@qq.com"
+git add -A
+git commit -m "modify post"
+git push git@github.com:gaoruich/blog-backup.git master
+pause
